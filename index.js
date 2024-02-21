488bdeb093bd8f7cb5dcfb53a1495fd4e73b3ce1
lucasxaviieer <Lucas.lima.xaviier@gmail.com>
2024-02-21 14:23:47 +0000

    Endpoint get '/item' criado

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello, World!')
 })
 
 app.get('/oi', function(req, res) {
     res.send('Olá, Mundo!')
+})
+
+// Lisa de personagens
+
+const lista = ["Rich Sanchez", "Morty Smith", "Summer Smith"]
+
+// ReadAll -> [GET] /item
+
+app.get('/item', function(req, res) {
+  res.send(lista)
 })
 
 app.listen(3000)
